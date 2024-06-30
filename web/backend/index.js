50d58d77a200ba7ba913717bc91dee0f220b1012
Akshith Kandivanam <akshithkandivanam@gmail.com>
2024-06-30 14:43:53 -0400

    Security changes in node backend + frontend

--- web/backend/index.js
+++ web/backend/index.js
@@ -1,23 +1,25 @@
-// index.js
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/logs', (req, res) => {
     const logMessage = req.body.message;
-    
-    try{
-        axios.post('http://log-collector-microservice:8080/log_collector', {
+
+    try {
+        axios.post(process.env.LOG_COLLECTOR_MICROSERVICE_URI, {
             message: logMessage
         });
-    }catch (error) {
-        console.error('Error processing log:');
-        res.status(500).send('Error processing log');
+    } catch (error) {
+        console.error('Error processing log:', error);
+        return res.status(500).send('Error processing log');
     }
     console.log('Received log:', logMessage);
     res.sendStatus(200);
--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
     setLogMessage(logMessage);
 
     try {
-      const response = await fetch('http://localhost:8000/logs', {
+      const response = await fetch(process.env.REACT_APP_NODE_BACKEND_URI, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
