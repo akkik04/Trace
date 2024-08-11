4a372e19c7651d0eec0377eae9612841b724f4c8
Akshith Kandivanam <akshithkandivanam@gmail.com>
2024-08-11 15:48:18 -0400

    Use AWS SDK for ECS IP retrieval in Collector service
    This commit updates the Collector service to utilize the aws-sdk-go library for retrieving the public IP associated with its ECS task. Previously, the service relied on static IP configurations, which posed scalability and maintenance challenges. By dynamically fetching the IP using the AWS SDK, the Collector service now adapts seamlessly to changes in ECS deployments. The change is pivotal as both the Collector and Ingestor services are deployed within the same ECS task definition. Leveraging the retrieved IP, the Collector service establishes a gRPC communication channel with the Ingestor service, ensuring robust inter-service communication over a designated port.

--- services/collector/main.go
+++ services/collector/main.go
@@ -13,6 +13,11 @@ import (
 	"time"
 
 	"github.com/akkik04/Trace/services/proto"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -46,6 +51,18 @@ var logRegex = regexp.MustCompile(`(?P<IP>\d+\.\d+\.\d+\.\d+) - (?P<User>[^\s]+)
 
 // function to handle the incoming logs.
 func logsHandler(w http.ResponseWriter, r *http.Request) {
+
+	// set CORS headers -- allow all origins, methods, and headers for the sake of simplicity.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	// handle preflight requests.
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 
 	// if the request method is not POST, return an error.
 	if r.Method != "POST" {
@@ -85,6 +102,23 @@ func logsHandler(w http.ResponseWriter, r *http.Request) {
 	// print the extracted log entry.
 	fmt.Printf("Extracted Log Entry: %+v\n", logEntry)
 
+	// get the IP address of the task (both the collector and ingestor are running as tasks in the same ECS service).
+	// so we can use the task IP to communicate with the ingestor service.
+	ecsIP, err := getTaskPublicIP()
+	if err != nil {
+		http.Error(w, "Error retrieving ECS Task Public-IP", http.StatusInternalServerError)
+		return
+	}
+
+	// construct the ingestor address (assuming port 8082), and create a gRPC connection to the ingestor service.
+	ingestorAddr := fmt.Sprintf("%s:8082", ecsIP)
+	conn, err := grpc.NewClient(ingestorAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Println("Error creating connection to ingestor: ", err)
+		panic(err)
+	}
+	client = proto.NewLogServiceClient(conn)
+
 	// store the log entry in the in-memory data store.
 	logStore = append(logStore, logEntry)
 	res, err := client.SendLog(context.Background(), &proto.LogEntry{
@@ -99,13 +133,14 @@ func logsHandler(w http.ResponseWriter, r *http.Request) {
 	})
 	if err != nil {
 		http.Error(w, "Error sending log to ingestor", http.StatusInternalServerError)
+		fmt.Println("Error sending log to ingestor: ", err)
 		return
 	}
 	fmt.Printf("Response from ingestor: %v\n", res.GetMessage())
 	w.WriteHeader(http.StatusOK)
 }
 
-// function to extract meaningful metrics from the log message.
+// helper function to extract meaningful metrics from the log message.
 func extractLogMetrics(log string) (LogEntry, error) {
 
 	// use the regex to extract the metrics.
@@ -146,24 +181,93 @@ func extractLogMetrics(log string) (LogEntry, error) {
 	return logEntry, nil
 }
 
+// helper function to get the public IP address of the ECS task.
+func getTaskPublicIP() (string, error) {
+
+	// load .env file.
+	err := godotenv.Load(".env")
+	if err != nil {
+		return "", fmt.Errorf("error loading .env file: %v", err)
+	}
+
+	// retrieve necessary environment variables.
+	accessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	region := os.Getenv("AWS_REGION")
+	clusterName := os.Getenv("AWS_ECS_CLUSTER_NAME")
+	serviceName := os.Getenv("AWS_ECS_SERVICE_NAME")
+
+	// initialize AWS session.
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String(region),
+		Credentials: credentials.NewStaticCredentials(accessKeyID, secretAccessKey, ""),
+	}))
+
+	// create ECS service client.
+	ecsSvc := ecs.New(sess)
+
+	// list the tasks for the specified service.
+	listTasksOutput, err := ecsSvc.ListTasks(&ecs.ListTasksInput{
+		Cluster:     aws.String(clusterName),
+		ServiceName: aws.String(serviceName),
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to list tasks: %v", err)
+	}
+
+	// ensure there is at least one task running.
+	if len(listTasksOutput.TaskArns) == 0 {
+		return "", fmt.Errorf("no tasks found for service %s in cluster %s", serviceName, clusterName)
+	}
+
+	// describe the task to get the ENI (Elastic Network Interface)
+	taskDesc, err := ecsSvc.DescribeTasks(&ecs.DescribeTasksInput{
+		Cluster: aws.String(clusterName),
+		Tasks:   listTasksOutput.TaskArns,
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to describe tasks: %v", err)
+	}
+
+	// create EC2 service client.
+	ec2Svc := ec2.New(sess)
+	var publicIP string
+
+	// extracting the ENI ID and retrieveing the public IP so that we can use it during gRPC comms w/ the ingestor service.
+	for _, task := range taskDesc.Tasks {
+		for _, attachment := range task.Attachments {
+			if *attachment.Type == "ElasticNetworkInterface" {
+				for _, detail := range attachment.Details {
+					if *detail.Name == "networkInterfaceId" {
+						eniID := *detail.Value
+
+						// describe the ENI to get the public IP address, if it exists.
+						eniDesc, err := ec2Svc.DescribeNetworkInterfaces(&ec2.DescribeNetworkInterfacesInput{
+							NetworkInterfaceIds: []*string{aws.String(eniID)},
+						})
+						if err != nil {
+							return "", fmt.Errorf("failed to describe network interface: %v", err)
+						}
+
+						if len(eniDesc.NetworkInterfaces) > 0 && eniDesc.NetworkInterfaces[0].Association != nil {
+							publicIP = *eniDesc.NetworkInterfaces[0].Association.PublicIp
+
+							// if the public IP is found, return it.
+							return publicIP, nil
+						}
+					}
+				}
+			}
+		}
+	}
+	return "", fmt.Errorf("public IP not found for any task in service %s", serviceName)
+}
+
 // main.
 func main() {
 
-	// load the environment variables.
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-	ingestor := os.Getenv("LOG_INGESTOR_MICROSERVICE_URI")
-
-	// run the /log_collector route on port 8080.
+	// run the /log_collector route on port 8080 to handle incoming requests from the web (frontend) simulator.
 	http.HandleFunc("/log_collector", logsHandler)
 	fmt.Println("Go microservice running on port 8080")
-	conn, err := grpc.NewClient(ingestor, grpc.WithTransportCredentials(insecure.NewCredentials()))
-
-	if err != nil {
-		panic(err)
-	}
-	client = proto.NewLogServiceClient(conn)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
